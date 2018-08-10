fd1ba7f1e9b60ba84ac30be2d042e6e0b9cff631
lpjiang97 <prestonj@cs.washington.edu>
2018-08-10 09:55:04 -0400

    read matlab file working version

--- scripts/convert.py
+++ scripts/convert.py
@@ -4,7 +4,7 @@ from biac2bids.prep.convert import run_all
 
 ### CHANGE HERE ###
 # TODO: check: 19425, 19239
-SUBJECTS = ['19492', '19636', '19671', '19418', '19480', '19357', '19761', '19589', '19658', '19497', '19338']
+SUBJECTS = ['19492', '19239', '19636', '19671', '19418', '19480', '19357', '19761', '19589', '19658', '19497', '19425', '19338']
 STUDY_PATH = "/Users/lpjiang/Research/CBT/"
 JSON_OUTPUT_PATH = "/Users/lpjiang/Research/CBT/JSONS/" 
 TRANSLATION_FILE_NAME = "series_order_note.txt" 
