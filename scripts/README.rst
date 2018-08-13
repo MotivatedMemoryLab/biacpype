c72c5bc1927cb018aa4dbc6a28efdf64d641f758
lpjiang97 <linxing.jiang@duke.edu>
2018-08-13 13:03:41 -0400

    add loading stdmaps

---
+++ scripts/README.rst
@@ -0,0 +1,8 @@
+==========================
+Using scripts instructions
+==========================
+
+- `convert.py`
+
+- `load_stdev_cluster.py`: This is for loading standard deviation maps and the cluster mask together into `fsleyes` for visual inspection.
+
---
+++ scripts/load_stdev_cluster.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+### CHANGE HERE TO YOUR STUDY FOLDER ###
+study_path = "/Volumes/adcock_lab/main/studies/CBT.01/"
+
+
+if len(sys.argv) != 2:
+    print("Usage: python load_stdev_cluster.py [subjectid]")
+    exit() 
+
+subject = sys.argv[1]
+
+mask = study_path + "from_kelvin/" + subject + "/localizer_cluster_ROIs/" + subject + "_sphere_zstat1_mask1_MNI2mm.nii.gz"
+stdev_path = study_path + "/data/func/" + subject + "/imageStats/" 
+stdev_maps = [stdev_path + i for i in os.listdir(stdev_path) if i.endswith("stdev.nii.gz") and i.startswith("bia5")]
+os.system("fsleyes " + mask + " " + " " .join(stdev_maps))
+
+
+
+
