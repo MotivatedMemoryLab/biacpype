eba5781ac0fa9239497216c51a5f945da4d434d4
lpjiang97 <prestonj@cs.washington.edu>
2018-08-21 10:35:09 -0400

    fix path

--- tests/prep/test_translation.py
+++ tests/prep/test_translation.py
@@ -1,10 +1,11 @@
+import os
 import unittest
 from biacpype.biac2bids.generate_json.translation import trans_dict
 
 class TranslationTest(unittest.TestCase):
 
     def test_trans(self):
-        d = trans_dict("notes.txt", "100", "tests/files/")
+        d = trans_dict("notes.txt",  "tests/files/func/100")
         assert d["4"] == "LOCALIZER"
 
 
