c1e549021dcde7898a6dc0eb8fd6758f88282a40
lpjiang97 <prestonj@cs.washington.edu>
2018-08-23 21:58:48 -0400

    fix test

--- tests/biac2bids/test_translation.py
+++ tests/biac2bids/test_translation.py
@@ -5,7 +5,7 @@ from biacpype.biac2bids.generate_json.translation import trans_dict
 class TranslationTest(unittest.TestCase):
 
     def test_trans(self):
-        d = trans_dict("tests/files/study_folder_valid/Data/Func/19357/series_order_note.txt")
+        d = trans_dict("tests/files/study_folder_valid/Data/Func/19357")
         assert d["4"] == "LOCALIZER"
 
 
