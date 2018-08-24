b354a9eaf9e36b448d8f281339fe97affc7968fe
lpjiang97 <prestonj@cs.washington.edu>
2018-08-23 22:00:58 -0400

    fix delimiter

--- tests/biac2bids/test_translation.py
+++ tests/biac2bids/test_translation.py
@@ -5,7 +5,7 @@ from biacpype.biac2bids.generate_json.translation import trans_dict
 class TranslationTest(unittest.TestCase):
 
     def test_trans(self):
-        d = trans_dict("tests/files/study_folder_valid/Data/Func/19357")
+        d = trans_dict("tests/files/study_folder_valid/Data/Func/19357", delimiter=",")
         assert d["4"] == "LOCALIZER"
 
 
