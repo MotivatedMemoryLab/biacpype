39e9acceff95371544ac2334faf66bf0025db299
lpjiang97 <prestonj@cs.washington.edu>
2018-09-01 16:36:31 -0400

    take command line input

--- scripts/convert_to_bids.py
+++ scripts/convert_to_bids.py
@@ -1,20 +1,23 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"))
+import argparse
 from biacpype.biac2bids.generate_json.convert import generate_all_json_files
 from biacpype.biac2bids.bxh2bids.run_bxh2bids import bxh_to_bids
 from biacpype.biac2bids.clean_names.clean_name import group_sessions
 from biacpype.util.constants import set_paths
 
-### CHANGE HERE ###
-STUDY_PATH = "tutorial/study"
-JSON_OUTPUT_PATH = "tutorial/study/JSONS" 
-BIDS_PATH = "tutorial/study/bids"
-LOG_PATH = "tutorial/study/logs"
+
+
+parser = argparse.ArgumentParser(description='validation')
+parser.add_argument('study_path', help='path to your study folder')
+parser.add_argument('json_path', help='path to output json files')
+parser.add_argument('bids_path', help='path to output BIDS data')
+parser.add_argument('log_path', help='path to output logs')
 
 # ---------pipe begins---------  # 
-
+args = parser.parse_args()
 # set paths
-set_paths(STUDY_PATH=STUDY_PATH, JSON_OUTPUT_PATH=JSON_OUTPUT_PATH, BIDS_PATH=BIDS_PATH, LOG_PATH=LOG_PATH)
+set_paths(STUDY_PATH=args.study_path, JSON_OUTPUT_PATH=args.json_path, BIDS_PATH=args.bids_path, LOG_PATH=args.log_path)
 # generate jsons
 generate_all_json_files()
 # convert to bids
--- scripts/validate_biac_study_folder.py
+++ scripts/validate_biac_study_folder.py
@@ -1,13 +1,15 @@
 import sys, os
 import logging
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"))
+import argparse
 from biacpype.util.validation import verify_biac_path
 from biacpype.util.constants import set_paths
 
 
-STUDY_PATH = "tutorial/study"
-
+parser = argparse.ArgumentParser(description='validation')
+parser.add_argument('study_path', help='path to your study folder')
 
 ### validation begins ###
-set_paths(STUDY_PATH=STUDY_PATH)
+args = parser.parse_args()
+set_paths(STUDY_PATH=args.study_path)
 verify_biac_path()
