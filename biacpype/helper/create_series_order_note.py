4f321b6af5238cbce3e714ac80dad6cb2214edff
lpjiang97 <prestonj@cs.washington.edu>
2018-09-03 15:56:54 -0400

    fix file name

--- biacpype/helper/create_series_order_note.py
+++ biacpype/helper/create_series_order_note.py
@@ -7,7 +7,7 @@ def populate_file(folder, dictionary, subjs=None):
     folder_path = os.path.join(Const.STUDY_PATH, "Data", folder)
     for folder in os.listdir(folder_path):
         if not folder.startswith(".") and include_subj(folder, subjs):
-            build_file(dictionary, os.path.join(folder_path, folder, "series_order_note_try.tsv"))
+            build_file(dictionary, os.path.join(folder_path, folder, "series_order_note.tsv"))
 
 
 def include_subj(subj, subjs):
